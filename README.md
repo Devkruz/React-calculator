0a74df608278ba2ff862ff9d66f6f9f2d7f58167
Sanni Adetola <tonyexplorer202@gmail.com>
2022-02-03 12:15:15 +0100

    Fix operator bug

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/edd152a9-accc-4edc-b7eb-becbb19416a4/deploy-status)](https://app.netlify.com/sites/sleepy-almeida-6ffbbb/deploys)
+
 # React Calculator
-## React Calculator is a app this help perform simple arithemetic calculation
+## React Calculator is a app that help perform simple arithmetic calculation
 ## This app is a certification project for Free-code-camp
 
 
@@ -11,3 +13,5 @@ This is a list of tools and libary i used.
 
 ![React Calculator](./calculator.png)
 Design inspiration [here:](https://dribbble.com/shots/3157278-Calculator-App-Design) 
+
+# **Feel free to contribute to my project, anything to better my code will be appreciated. Thank you**
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-//Array of operators
+// Array of operators
 const operators = [
     {
         name: "add",
@@ -18,7 +18,7 @@ const operators = [
         symbol: "*"
     }
 ];
-//Array of methods
+// Array of methods
 const methods = [
     {
         name: "clear",
@@ -37,7 +37,7 @@ const methods = [
         symbol: "="
     }
 ];
-//Array of numbers
+// Array of numbers
 const numbers = [
     {
         name: "zero",
@@ -80,6 +80,7 @@ const numbers = [
         symbol: "9"
     }      
 ];
+// RegEx to check for more than three operator in a row
 const operatorRegEx = /[-+/*]{3,}/g;
 
 
@@ -89,78 +90,74 @@ const operatorRegEx = /[-+/*]{3,}/g;
            super()
            this.state = {
                calculation: "0",
-               result: "",
-               keyInput: ""
+               result: ""
            };
-           
+        
            this.methodHandler = this.methodHandler.bind(this); 
            this.numberHandler = this.numberHandler.bind(this);
            this.operatorHandler = this.operatorHandler.bind(this);
 
       }
-       //Method to handle the calculation operators
+       // Method to handle the calculation operators
       methodHandler(symbol) {
+          // Arr of operators to check the incoming input
         const operatorsArr = ["+", "-", "/", "*"]
-            if ( symbol == "Del") {
-                const copy = this.state.calculation.slice(0).split('');              
+            // Check if the symbol is "Del"
+            if ( symbol == "Del") { 
+                // copy the calcultion state to perfome mutable functions
+                const copy = this.state.calculation.slice(0).split('');  
+                // Delete the last digit            
                 const clear =  copy.splice(-1,1).join("");
-            
+                // Set the state when the last digit is deleted
                 this.setState({
                     calculation: copy.join("")
                 });
-
+                // Once the last digit is deleted, check if the new last digit is not an operator 
+                // Evaluate and set it to the result state
                 if (!operatorsArr.includes(copy.join("").slice(-1))) {
                     console.log("active")
                     this.setState({
                        result: eval(copy.join(""))
                     })
                 };
+                // Check if  the calculation state is empty
+                // Set the calculation state back to it initiall state "0"
                 if (this.state.calculation.length <= 1) {
                     this.setState({
                         calculation: "0"
                     });
                 }
             }
+            // Check if the symbol is "Clear"
             else if (symbol == "C") {
+                // Set the calculation state to "0"
+                // Set the result state to ""
                    this.setState({
                         calculation: "0",
                         result: ""
                    })
             }
+            // Check if the symbol is "="
             else if (symbol == "=") {
-                const op = [...this.state.calculation.matchAll(operatorRegEx)];
-                let reps = this.state.calculation;
-                if(op.length > 0) {
-                    op.forEach(opReg => {
-                        opReg.forEach(reg => {
-                        const  rep = reg.slice(-1);
-                        reps = reps.replace(opReg,rep);
-                        });
-                    });
-                    
-                         console.log(eval(reps))
-                        
-                }
-                else {
+                   // Set the calculation state to result state
                     this.setState({
                         calculation: this.state.result.toString()
                       });
-                }    
             }
+            // Check if the symbol is "."
             else if (symbol = ".") {
-                if (this.state.calculation.length <= 0) {
-                      this.setState({
-                           calculation: "0."
-                      });
-                }
-                else {
+                // Check if the last digit is a decimal
                     if(this.state.calculation.slice(-1) == ".") {
                        return;
                     } 
-                    this.setState({
-                        calculation: this.state.calculation + "."
-                   }) 
-                }
+                // If not
+                    else {
+                        this.setState({
+                            calculation: this.state.calculation + "."
+                        });
+                    }
+                    
+                
             }
          
             
@@ -168,81 +165,119 @@ const operatorRegEx = /[-+/*]{3,}/g;
       };
       //Method to handle the calculation digits
       numberHandler(symbol) { 
-             
+               // Check if the calculation state length is less than 15
                 if(this.state.calculation.length <= 15) {
+                    // Check if the calculation state is "0"
+                    // Check if the symbol is "0"
                     if (this.state.calculation == "0" &&  symbol == "0") {
+                        // Set the calculatio stae to "0"
                          this.setState({
                               calculation: "0"
                          })
                     };
+                    // Check if the calcultion stae is "0"
+                    // Check if the symbol is not "0"
                     if (this.state.calculation == "0" && !symbol == "0") {
+                        // Set the calculation state to the incoming symbol
                         this.setState({ 
                             calculation: symbol   
                         });
                     }
                     else {
-                        this.setState({ 
-                            calculation:this.state.calculation + symbol,
-                            result: eval(this.state.calculation + symbol)
-                        });
+                        // Match all consecutive operator that are more than three
+                        // Put them into an array
+                        const op = [...this.state.calculation.matchAll(operatorRegEx)];
+                        // Concat the calculation state and symbol
+                        // Put it into a variable
+                        let reps = this.state.calculation + symbol;
+                        // Check if operator array is filled 
+                        if(op.length > 0) {
+                            // Loop through the operator array
+                            op.forEach(opReg => {
+                                // Loop through the operator string
+                                opReg.forEach(reg => {
+                                // Put the last operator into a variable
+                                const  rep = reg.slice(-1);
+                                // Replace the string of operator with the last operator
+                                // Put it in the declared variable
+                                reps = reps.replace(opReg,rep);
+                                });
+                            });
+                            // Set the calculation state 
+                            // Evaluate the mutated calcultation and set it to the result state
+                            this.setState({ 
+                                calculation:this.state.calculation + symbol,
+                                result: eval(reps)
+                            });
+                            console.log(typeof(reps))
+                            console.log(reps)
+                            console.log("operation filled")
+
+                        }
+                        // If the operator array is not filled
+                        else {
+                            // Set the calculation state and the result state
+                            this.setState({ 
+                                calculation:this.state.calculation + symbol,
+                                result: eval(this.state.calculation + symbol)
+                            });
+                            console.log("operation not filled")
+                        }
                     };
                 };
+                // Check if the calcultion state lenght is greater than 15 
                 if(this.state.calculation.length > 15) {
+                    // Set the result state to "Max. Digit"
                     this.setState({ 
                         result: "Max. Digit"
                     });
 
                  }
       };
-      //Method to handle the calculation functions
+      //Method to handle the calculation functions 
       operatorHandler(symbol) {
+                // Operator Array
                 const operatorsArr = ["+", "-", "/", "*"]
                 const validOperatorsArr = ["-+", "+-", "--", "++", "/-", "/+", "*-", "*+"] 
               
-                    
+                        // Check if the calculation state is "0"
                         if(this.state.calculation == "0") {
+                            // Check if the symbol is "+" or "-"
                             if(symbol == "+" || symbol == "-")
+                            // Set the calculation state to the symbol
                             this.setState({ 
                                 calculation:symbol
                             });
                         }
+                        // If the calculation state is not "0"
                         else {
+                            // Check if the last digit in the calculation is operator
                             if(operatorsArr.includes(this.state.calculation.slice(-1))) {
+                                // Check if the symbol is "/" or "*"
                                 if(symbol == "/" || symbol == "*") {
+                                    // Do nothing
                                     return;
                                 }
+                                // If not the symbol is  "/" or "*"
                                 else {
-                                    this.setState({ 
-                                        calculation: this.state.calculation + symbol
-                                    });
+                                    // Check if the last digit in the calculation state is equal to the symbol
+                                    if(this.state.calculation.slice(-1) == symbol) {
+                                        // Do nothing
+                                        return;
                                     }
-                               
-
-
-    
-
-
-
-                                // else {
-                                    // if(validOperatorsArr.includes(this.state.calculation.slice(-2))) {
-                                    //     return;
-                                    // }
-                                    // else {
-                                    // this.setState({ 
-                                    //     calculation: this.state.calculation + symbol
-                                    // });
-                                    // }
-
-                                //    const calCopy = this.state.calculation.slice(0,-1).split('').concat(symbol).join('');
-                                //    this.setState({
-                                //       calculation:calCopy,
-                                //    });
-
-
-                                // };
+                                    else {
+                                        // If not
+                                        this.setState({ 
+                                            // Add symbol to what is in the calculation state
+                                            calculation: this.state.calculation + symbol
+                                        });
+                                    }
+                                    };
                              }
+                             // If not the last digit in calculation state is an operator
                              else {
                                 this.setState({ 
+                                    // Add symbol to what in the calculation state
                                     calculation: this.state.calculation + symbol
                                 });
                              }             
@@ -253,16 +288,25 @@ const operatorRegEx = /[-+/*]{3,}/g;
 
 
   render() {
+        // Declare a number array
         const number = [];
+        // Loops through numbers array 
         numbers.forEach((num,index)=> {
+            // Push to number array with this data
             number.push(<KeyPad calcHandler={this.numberHandler} symbol={num.symbol} key={num.symbol} class="number" id={num.name} />)
         });
-        const operator =[];
+        // Declare an operator array
+        const operator = [];
+        // Loops through operators array 
         operators.forEach((opera, index)=> {
+            // Push to operator array with this data
             operator.push(<KeyPad calcHandler={this.operatorHandler} symbol={opera.symbol} key={index} class="operators" id={opera.name} />)
         });
+        // Declare an method array
         const method = [];
+         // Loops through method array 
         methods.forEach((meth, index)=> {
+             // Push to method array with this data
             method.push(<KeyPad calcHandler={this.methodHandler} symbol={meth.symbol} key={index} class="operators" id={meth.name} />)
         })
        
@@ -319,7 +363,7 @@ class KeyPad extends React.Component {
                         this.activeControl = this.activeControl.bind(this);
                    }
 
-
+              
                    clickHandler() { 
                     this.props.calcHandler(this.props.symbol);
                     this.activeControl();
